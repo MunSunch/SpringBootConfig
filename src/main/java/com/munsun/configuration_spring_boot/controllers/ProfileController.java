08d24a4d3decd8c01b1e0c4b8b7171f47e15ff51
MunirTheBest <msunchalyaev@gmail.com>
2023-09-13 14:31:51 +0300

    finish task nginx-reverse-proxy

--- src/main/java/com/munsun/configuration_spring_boot/controllers/ProfileController.java
+++ src/main/java/com/munsun/configuration_spring_boot/controllers/ProfileController.java
@@ -1,11 +1,12 @@
 package com.munsun.configuration_spring_boot.controllers;
 
 import com.munsun.configuration_spring_boot.profile.SystemProfile;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class ProfileController {
     private SystemProfile profile;
@@ -17,6 +18,7 @@ public class ProfileController {
 
     @GetMapping("/profile")
     public String profile() {
+        log.info("GET /profile");
         return profile.getProfile();
     }
 }
